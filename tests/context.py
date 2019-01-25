0b6154a0caca115dd98ba11b95406e4133455728
Sean Reifschneider <sean@realgo.com>
2019-01-25 08:35:21 -0700

    Removing unused test context

--- tests/context.py
+++
@@ -1,8 +0,0 @@
-#  From: https://docs.python-guide.org/writing/structure/
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
-
-import spawk
