2f063c4922687cf9e50c0a4cfb295bd397e1cea2
Sean Reifschneider <jafo00@gmail.com>
2019-01-27 18:10:54 -0700

    Adding a test of eval

--- tests/test_apache.py
+++ tests/test_apache.py
@@ -31,3 +31,15 @@ class TestApacheLog(TestCase):
         self.assertEqual(
                 ''.join(self.t.context.data),
                 '200200200404404200200')
+
+    def test_eval(self):
+        self.t.context.bytes_tx = 0
+
+        @self.t.eval('line["status"] == "200"')
+        def logent(context, record):
+            context.bytes_tx += int(record['bytes_tx'])
+        self.t.run()
+
+        self.assertEqual(self.t.context.bytes_tx, 35223)
+
+#dict_keys(['remote_host', 'remote_logname', 'remote_user', 'time_received', 'time_received_datetimeobj', 'time_received_isoformat', 'time_received_tz_datetimeobj', 'time_received_tz_isoformat', 'time_received_utc_datetimeobj', 'time_received_utc_isoformat', 'request_first_line', 'request_method', 'request_url', 'request_http_ver', 'request_url_scheme', 'request_url_netloc', 'request_url_path', 'request_url_query', 'request_url_fragment', 'request_url_username', 'request_url_password', 'request_url_hostname', 'request_url_port', 'request_url_query_dict', 'request_url_query_list', 'request_url_query_simple_dict', 'status', 'bytes_tx', 'request_header_referer', 'request_header_user_agent', 'request_header_user_agent__browser__family', 'request_header_user_agent__browser__version_string', 'request_header_user_agent__os__family', 'request_header_user_agent__os__version_string', 'request_header_user_agent__is_mobile'])
